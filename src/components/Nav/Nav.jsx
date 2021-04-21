82a60389560179f4b144834162944a2209542a8e
MatthewHeinrich <mheinrich95@gmail.com>
2021-04-21 08:54:27 -0500

    trail names and maps rendered

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -20,7 +20,7 @@ function Nav() {
   return (
     <div className="nav">
       <Link to="/home">
-        <h2 className="nav-title">Prime Solo Project</h2>
+        <h2 className="nav-title">Trax</h2>
       </Link>
       <div>
         <Link className="navLink" to={loginLinkData.path}>
--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -6,13 +6,21 @@ function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   const user = useSelector((store) => store.user);
   const trails = useSelector((store) => store.trails)
+  
   return (
-    <div className="container">
+    <>
       <h2>Welcome, {user.username}!</h2>
       <p>Your ID is: {user.id}</p>
-      <p>{JSON.stringify(trails)}</p>
-      <LogOutButton className="btn" />
-    </div>
+    {trails.map(trail => {
+      return (
+        <div className="container">
+          <p>{trail.trail_name}</p>
+          <p>{trail.trail_city}</p>
+          <img src={trail.map_url}></img>
+        </div>
+      );
+    })}
+    </>
   );
 }
 
